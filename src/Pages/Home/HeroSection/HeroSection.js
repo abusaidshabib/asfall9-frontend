405ed89da333d71fe9b819d9e4060205b1f935bc
Error <error@Error.Error>
2022-11-26 06:50:19 +0600

    hover menu

--- src/Pages/Home/HeroSection/HeroSection.js
+++ src/Pages/Home/HeroSection/HeroSection.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const HeroSection = () => {
 
@@ -13,7 +14,7 @@ const HeroSection = () => {
                             Your best choice<br />
                         </h1>
                         <p className='text-xl text-white pt-8 pb-10'>You can find the best reseller car for yourself. All the payment and other important things are heavy secure.</p>
-                        <button className="btn btn-primary">Get Started</button>
+                        <Link className="btn colorGray border-none rounded-none bg-colorYellow bg-colorYellowDk mr-5" > sell all products</Link>
                     </div>
                 </div>
             </div>
